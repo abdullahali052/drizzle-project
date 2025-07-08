73392e2c77aae1ea4736920f347db5b59a7c4bee
abdullahali052 <abdullah202301032005@gmail.com>
2025-07-08 08:52:01 +0500

    Modify: added zod validation to env variables

--- config/env.js
+++ config/env.js
@@ -1,9 +1,8 @@
-// import z from "zod";
+import z from "zod";
 
-// export const env = z
-//   .object({
-//     PORT: z.coerce.number().default(3000),
-//     MONGODB_URI: z.string(),
-//     MONGODB_DATABASE_NAME: z.string(),
-//   })
-//   .parse(process.env);
+export const env = z
+  .object({
+    PORT: z.coerce.number().default(3000),
+    DATABASE_URL: z.string(),
+  })
+  .parse(process.env);
