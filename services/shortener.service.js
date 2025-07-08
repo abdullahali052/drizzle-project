f42860cbc808a344927bc801a526737c9eaab6ca
abdullahali052 <abdullah202301032005@gmail.com>
2025-07-08 08:07:50 +0500

    Modify: added drizzle orm

--- services/shortener.service.js
+++ services/shortener.service.js
@@ -1,62 +1,34 @@
-import { PrismaClient } from "@prisma/client";
+import { eq } from "drizzle-orm";
+import { db } from "../config/db-config.js";
+import { shortLink } from "../drizzle/schema.js";
 
-const prisma = new PrismaClient();
-
-export const loadLinks = async () => {
-  const allShortCodeLinks = await prisma.shortLink.findMany();
+export const getAllShortLinks = async () => {
+  const allShortCodeLinks = await db.select().from(shortLink);
 
   return allShortCodeLinks;
 };
 
 export const getLinkByShortCode = async (shortCode) => {
-  //   const [rows] = await db.execute(
-  //     "select * from short_links where short_code = ?",
-  //     [shortCode]
-  //   );
+  const [shortLinkOne] = await db
+    .select()
+    .from(shortLink)
+    .where(eq(shortLink.shortCode, shortCode));
 
-  //   if (rows.length > 0) {
-  //     return rows[0];
-  //   } else {
-  //     return null;
-  //   }
-
-  const shortLink = await prisma.shortLink.findUnique({
-    where: { shortCode },
-  });
-
-  return shortLink;
+  return shortLinkOne;
 };
 
 export const saveLinks = async ({ url, shortCode }) => {
-  //   const [rows] = await db.execute(
-  //     "insert into short_links(short_code, url) values(?,?)",
-  //     [shortCode, url]
-  //   );
-
-  //   return rows;
-
-  console.log("url ", url, "shortcode ", shortCode);
-
-  const savelink = await prisma.shortLink.create({
-    data: { shortCode, url },
+  const saveLink = await db.insert(shortLink).values({
+    url,
+    shortCode,
   });
 
-  return savelink;
+  return saveLink;
 };
 
 export const deleteData = async (id) => {
-  //   try {
-  //     const [result] = await db.execute("DELETE FROM short_links WHERE id = ?", [
-  //       id,
-  //     ]);
-  //     return result.affectedRows > 0;
-  //   } catch (error) {
-  //     console.error("Delete error:", error);
-  //     throw error;
-  //   }
-
-  const deleteLink = await prisma.shortLink.delete({
-    where: { id },
+  const deleteLink = await db.delete(shortLink).where({
+    id,
   });
   return deleteLink;
 };
