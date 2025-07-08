e751919694372ba9fb4f956b62255109ff8c44ae
abdullahali052 <abdullah202301032005@gmail.com>
2025-07-08 08:04:41 +0500

    Added: auth routes

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 import { shortnerRoutes } from "./routes/shortner.routes.js";
-import path from "path";
 import express from "express";
 const app = express();
-
-const DATA_FILE = path.join("data", "links.json");
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 //express router
+app.use(authRoutes);
 app.use(shortnerRoutes);
 
 const port = process.env.PORT;
